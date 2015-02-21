9197037916be88664b634fdbb25d5eed2c219a64
lsm1987 <1987lsm@gmail.com>
2015-02-21 14:25:05 +0900

    플레이어기 충돌 외양 파티클에 Sorting Layer 지정할 수 있도록 수정

---
+++ Assets/Scripts/Game/ShapeParticleSortingLayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+// 파티클에 스프라이트 소팅 지정
+public class ShapeParticleSortingLayer : MonoBehaviour
+{
+    [SerializeField]
+    private string _sortingLayerName;
+    [SerializeField]
+    private int _sortingOrder;
+
+    private void Start()
+    {
+        if (particleSystem != null)
+        {
+            particleSystem.renderer.sortingLayerName = _sortingLayerName;
+            particleSystem.renderer.sortingOrder = _sortingOrder;
+        }
+    }
+}
