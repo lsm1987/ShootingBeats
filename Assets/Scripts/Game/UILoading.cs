84a5bb2fbfdca4f25867ca1c97d68207d2f3af56
lsm1987 <1987lsm@gmail.com>
2015-04-12 21:02:16 +0900

    UILoading 을 Game namespace 아래로 옮김

--- Assets/Scripts/Game/UILoading.cs
+++ Assets/Scripts/Game/UILoading.cs
@@ -1,41 +1,44 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-// 게임 씬 로딩
-public class UILoading : UIWindow
+namespace Game
 {
-    [SerializeField]
-    private Text _progress; // 진행상태
-
-    public override bool OnKeyInput()
+    // 게임 씬 로딩
+    public class UILoading : UIWindow
     {
-        // 다른 UI로 입력 넘어가지 못하게 막음
-        return true;
-    }
+        [SerializeField]
+        private Text _progress; // 진행상태
 
-    /// <summary>
-    /// 진행상태 문자열 지정
-    /// </summary>
-    /// <param name="text"></param>
-    public void SetProgress(string text)
-    {
-        _progress.text = text;
-    }
+        public override bool OnKeyInput()
+        {
+            // 다른 UI로 입력 넘어가지 못하게 막음
+            return true;
+        }
 
-    /// <summary>
-    /// 창 열기
-    /// </summary>
-    public void Open()
-    {
-        _progress.text = string.Empty;
-        gameObject.SetActive(true);
-    }
+        /// <summary>
+        /// 진행상태 문자열 지정
+        /// </summary>
+        /// <param name="text"></param>
+        public void SetProgress(string text)
+        {
+            _progress.text = text;
+        }
 
-    /// <summary>
-    /// 창 닫기
-    /// </summary>
-    public void Close()
-    {
-        gameObject.SetActive(false);
+        /// <summary>
+        /// 창 열기
+        /// </summary>
+        public void Open()
+        {
+            _progress.text = string.Empty;
+            gameObject.SetActive(true);
+        }
+
+        /// <summary>
+        /// 창 닫기
+        /// </summary>
+        public void Close()
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
