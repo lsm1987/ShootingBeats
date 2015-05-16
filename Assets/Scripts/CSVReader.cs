4d53b37c175c88e9770c033be61398ef57ea08c3
lsm1987 <1987lsm@gmail.com>
2015-05-16 13:26:42 +0900

    CSV 읽어들이는 클래스 추가

---
+++ Assets/Scripts/CSVReader.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+/// <summary>
+/// CSV 읽기
+/// </summary>
+public class CSVReader
+{
+    private string[] _lines; // 텍스트 어셋으로부터 읽어들인 문자열을 줄 단위로 저장
+    private int _nextReadLine = 0; // ReadLine() 에서 몇 번째 라인을 읽을 차례인가?
+    private List<string> _readLine = new List<string>(); // ReadLine() 에서 읽어들인 한 줄
+
+    public CSVReader(string path)
+    {
+        TextAsset asset = Resources.Load(path) as TextAsset;
+        if (asset == null)
+        {
+            Debug.LogError("[CSVReader] Invalid path:" + path);
+            return;
+        }
+
+        string text = asset.text;
+        text = text.Trim().Replace("\r", "") + "\n";
+        _lines = text.Split('\n');
+        _nextReadLine = 0;
+        _readLine.Clear();
+    }
+    
+    public string[] ReadLine()
+    {
+        if (_lines == null)
+        {
+            return null;
+        }
+
+        while (_nextReadLine < _lines.Length)
+        {
+            string line = _lines[_nextReadLine];
+            if (string.IsNullOrEmpty(line) || line.StartsWith("//"))
+            {
+                // 빈 줄
+                _nextReadLine++;
+                continue;
+            }
+
+            _readLine.Clear();
+
+            _nextReadLine++;
+            return ParseLine(line);
+        }
+
+        return null;
+    }
+
+    private string[] ParseLine(string line)
+    {
+        string pattern = @"
+             # Match one value in valid CSV string.
+             (?!\s*$)                                      # Don't match empty last value.
+             \s*                                           # Strip whitespace before value.
+             (?:                                           # Group for value alternatives.
+               '(?<val>[^'\\]*(?:\\[\S\s][^'\\]*)*)'       # Either $1: Single quoted string,
+             | ""(?<val>[^""\\]*(?:\\[\S\s][^""\\]*)*)""   # or $2: Double quoted string,
+             | (?<val>[^,'""\s\\]*(?:\s+[^,'""\s\\]+)*)    # or $3: Non-comma, non-quote stuff.
+             )                                             # End group of value alternatives.
+             \s*                                           # Strip whitespace after value.
+             (?:,|$)                                       # Field ends on comma or EOS.
+             ";
+
+        string[] values = (from Match m in Regex.Matches(line, pattern,
+                               RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline)
+                           select m.Groups[1].Value).ToArray();
+
+        for (int i = 0; i < values.Length; ++i)
+        {
+            values[i] = values[i].Replace("\\\"", "\"")
+                .Replace("\\n", "\n");
+        }
+
+        return values;
+    }
+}
