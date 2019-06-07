def9c8fbce21c011d81eafd8e28bead2629b43d2
lsm1987 <1987lsm@gmail.com>
2019-06-07 21:17:43 +0900

    [Calc] 1. 기본 파일들 추가

---
+++ Assets/Scripts/Game/Calc/Boss.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Game
+{
+    namespace Calc
+    {
+        // 보스
+        public class Boss : Enemy
+        {
+            private const int _score = 10; // 피격시 획득 점수
+            private CoroutineManager _coroutineManager = new CoroutineManager();
+
+            public Boss()
+                : base()
+            {
+            }
+
+            public override void Init(string shapeSubPath, float x, float y, float angle)
+            {
+                base.Init(shapeSubPath, x, y, angle);
+                _coroutineManager.RegisterCoroutine(MoveMain());
+            }
+
+            public override void Move()
+            {
+                _coroutineManager.UpdateAllCoroutines();
+            }
+
+            // 메인 코루틴
+            private IEnumerator MoveMain()
+            {
+                // 등장
+                _coroutineManager.StartCoroutine(_Logic.MoveConstantVelocity(this, new Vector2(0.0f, 0.75f), 120));
+                
+
+                // 폭발
+                yield return new WaitForAbsFrames(8700);
+                {
+                    Effect crashEffect = GameSystem._Instance.CreateEffect<Effect>();
+                    crashEffect.Init("Common/Effect_BossCrashMiku", _X, _Y, 0.0f);
+                }
+                _alive = false;
+            }
+
+            // 피격시
+            public override void OnHit()
+            {
+                GameSystem._Instance.SetScoreDelta(_score);
+            }
+
+            public override void OnDestroy()
+            {
+                base.OnDestroy();
+                _coroutineManager.StopAllCoroutines();
+            }
+
+            private GameLogic _Logic
+            {
+                get { return GameSystem._Instance.GetLogic<GameLogic>(); }
+            }
+        } // Boss
+    } // Calc
+} // Game
---
+++ Assets/Scripts/Game/Calc/GameLogic.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Game
+{
+    namespace Calc
+    {
+        // "Calc." 진행정보
+        public class GameLogic : Game.BaseGameLogic
+        {
+            private CoroutineManager _coroutineManager = new CoroutineManager();
+
+            // 특화 정보 로딩
+            public override IEnumerator LoadContext()
+            {
+                IEnumerator loadPlayer = LoadBasicPlayer();
+                while (loadPlayer.MoveNext())
+                {
+                    yield return loadPlayer.Current;
+                }
+
+                // 적기 로딩 /////////////////////
+                GameSystem._Instance.PoolStackShape("Common/Boss_Miku", 1);
+                GameSystem._Instance.PoolStackMover<Boss>(1);
+                GameSystem._Instance.PoolStackShape("Common/Effect_BossCrashMiku", 1);
+                GameSystem._Instance.PoolStackMover<Effect>(1);
+
+                // 탄 로딩 ///////////////////
+                // 외양 로딩
+                GameSystem._Instance._UILoading.SetProgress("Loading Bullets");
+                yield return null;
+                GameSystem._Instance.PoolStackShape("Common/Bullet_Blue", 270);
+                GameSystem._Instance.PoolStackShape("Common/Bullet_Red", 27);
+                
+                // 클래스 로딩
+                GameSystem._Instance.PoolStackMover<Bullet>(270);
+
+                // 코루틴
+                _coroutineManager.StopAllCoroutines();
+                _coroutineManager.RegisterCoroutine(Main());    // 메인 코루틴 등록
+            }
+
+            // 특화 정보 갱신
+            public override void UpdatePlayContext()
+            {
+                _coroutineManager.UpdateAllCoroutines();
+            }
+
+            private IEnumerator Main()
+            {
+                // 플레이어 생성
+                PlayerAlive player = GameSystem._Instance.CreatePlayer<PlayerAlive>();
+                player.Init("Common/Player_Black", 0.0f, -0.7f, 0.0f);
+
+                // 보스 생성
+                Boss boss = GameSystem._Instance.CreateEnemy<Boss>();
+                boss.Init("Common/Boss_Miku", -0.5f, 1.3f, 0.0f);
+
+                yield return null;
+            }
+
+            #region Coroutine
+            #endregion Coroutine
+        } // GameLogic
+    } // Calc
+} // Game
