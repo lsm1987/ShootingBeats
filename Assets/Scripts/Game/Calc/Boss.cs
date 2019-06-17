dc09fa720c98a6a9617bcc6a73ffa977b7e44d25
lsm1987 <1987lsm@gmail.com>
2019-06-17 23:28:29 +0900

    [Calc] 1. 진자 원 패턴 세로선 구현

--- Assets/Scripts/Game/Calc/Boss.cs
+++ Assets/Scripts/Game/Calc/Boss.cs
@@ -242,13 +242,8 @@ namespace Game
                 const float rotationRadius = 0.6f;
                 const float moverStartRad = (Mathf.PI / 2.0f); // 90도 위치부터 시작
 
-                // 라인 메모
-                // 본체 y=0.6sin((pi/2)+((2pi)/320)t)+0.15
-                // 라인 y=-0.04t+1.3
-                // t=14.3407
-
                 _coroutineManager.StartCoroutine(Pattern_Pendulum_Move(rotationCount, rotationDuration, rotationRadius, moverStartRad, true));
-                _coroutineManager.StartCoroutine(Pattern_Pendulum_Line(rotationCount, rotationDuration, rotationRadius, moverStartRad));
+                _coroutineManager.StartCoroutine(Pattern_CirclePendulum_Line(rotationCount, rotationDuration, rotationRadius, moverStartRad));
                 _coroutineManager.StartCoroutine(Pattern_Pendulum_NWay());
             }
 
@@ -313,6 +308,139 @@ namespace Game
                     }
                 }
             }
+
+            private IEnumerator Pattern_CirclePendulum_Line(int rotationCount, int rotationDuration, float rotationRadius, float moverStartRad)
+            {
+                // n번째로 쏜 라인 탄이 몇 프레임에 본체와 같은 Y 위치가 되는가?
+                // 본체: y=0.6sin((pi/2)+((2pi)/320)t)+0.15
+                // 라인: y=-0.04(t-4n)+1.3
+                float[] pendulumFramePerLineInteval = {
+                    14.3407f    // 0
+                    , 18.7557f
+                    , 23.2915f
+                    , 27.9532f
+                    , 32.745f
+                    , 37.6693f
+                    , 42.726f
+                    , 47.9123f
+                    , 53.2215f
+                    , 58.6424f
+                    , 64.1594f  // 10
+                    , 69.7521f
+                    , 75.3958f
+                    , 81.0631f
+                    , 86.7249f
+                    , 92.3526f
+                    , 97.9195f
+                    , 103.403f
+                    , 108.783f
+                    , 114.047f
+                    , 119.186f  // 20
+                    , 124.193f
+                    , 129.067f
+                    , 133.81f
+                    , 138.424f
+                    , 142.914f
+                    , 147.285f
+                    , 151.544f
+                    , 155.696f
+                    , 159.75f
+                    , 163.71f   // 30
+                    , 167.584f
+                    , 171.377f
+                    , 175.096f
+                    , 178.745f
+                    , 182.331f
+                    , 185.858f
+                    , 189.331f
+                    , 192.753f
+                    , 196.131f
+                    , 199.467f  // 40
+                    , 202.765f
+                    , 206.03f
+                    , 209.263f
+                    , 212.469f
+                    , 215.651f
+                    , 218.812f
+                    , 221.954f
+                    , 225.081f
+                    , 228.196f
+                    , 231.3f    // 50
+                    , 234.397f
+                    , 237.489f
+                    , 240.579f
+                    , 243.67f
+                    , 246.764f
+                    , 249.863f
+                    , 252.971f
+                    , 256.09f
+                    , 259.222f
+                    , 262.371f  // 60
+                    , 265.539f
+                    , 268.73f
+                    , 271.946f
+                    , 275.191f
+                    , 278.467f
+                    , 281.779f
+                    , 285.13f
+                    , 288.525f
+                    , 291.966f
+                    , 295.458f  // 70
+                    , 299.006f
+                    , 302.615f
+                    , 306.29f
+                    , 310.036f
+                    , 313.859f
+                    , 317.764f
+                    , 321.759f
+                    , 325.849f
+                    , 330.041f
+                };
+
+                float[] offsetXs = new float[2] { -0.4f, 0.4f };
+                const int interval = 4;
+                const string shape = "Common/Bullet_Blue";
+                const float fireStartY = 1.30f;
+                const float angle = 0.75f;
+                const float speed = 0.04f;
+
+                float pendulumCenterX = _X;
+                float radSpeed = (Mathf.PI * 2.0f / rotationDuration);
+
+                for (int rotation = 0; rotation < rotationCount; ++rotation)
+                {
+                    for (int rotationFrame = 0; rotationFrame < rotationDuration; ++rotationFrame)
+                    {
+                        if ((rotationFrame % interval) == 0)
+                        {
+                            int pendulumFrameIdx = rotationFrame / interval;
+
+                            if (pendulumFrameIdx < pendulumFramePerLineInteval.Length)
+                            {
+                                float pendulumFrame = pendulumFramePerLineInteval[pendulumFrameIdx];
+                                float pendulumRad = moverStartRad + radSpeed * pendulumFrame;
+                                float pendulumX = pendulumCenterX + rotationRadius * Mathf.Cos(pendulumRad);
+
+                                foreach (float offsetX in offsetXs)
+                                {
+                                    Bullet b = GameSystem._Instance.CreateBullet<Bullet>();
+                                    b.Init(shape, pendulumX + offsetX, fireStartY, angle, 0.0f, speed, 0.0f);
+                                }
+                            }
+                            else
+                            {
+                                Debug.LogError(
+                                    string.Format("Invalid Idx. pendulumFrameIdx: {0}, pendulumFramePerLineInteval.Length: {1}"
+                                    , pendulumFrameIdx, pendulumFramePerLineInteval.Length)
+                                    );
+                            }
+                        }
+
+                        yield return null;
+                    }
+                }
+            }
+
 
             private IEnumerator Pattern_Pendulum_NWay()
             {
