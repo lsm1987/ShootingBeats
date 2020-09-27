35bd75f9fc93609d5a219e1f01310f6499372ec9
lsm1987 <1987lsm@gmail.com>
2020-09-27 23:10:52 +0900

    [Localization] 타이틀 씬의 텍스트들이 다국어 대응하도록 수정

--- Assets/Scripts/TitleSystem.cs
+++ Assets/Scripts/TitleSystem.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Tables;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -7,9 +9,9 @@ public class TitleSystem : SceneSystem
     [SerializeField]
     private Button _btnStart = null;
     [SerializeField]
-    private Text _btnTextStart = null;
-    private const string _textStart = "Start";  // 게임 시작
-    private const string _textSignInDoing = "Sign In...";   // 로그인 도중
+    private LocalizeStringEvent _strEvtStart = null;
+    private readonly TableEntryReference _strKeyStart = "Menu_Start";   // 게임 시작
+    private readonly TableEntryReference _strKeySignInDoing = "Menu_SignIn";   // 로그인 도중
     [SerializeField]
     private Button _btnAchievement = null;
 
@@ -79,7 +81,7 @@ public class TitleSystem : SceneSystem
     {
         // 시작 버튼
         _btnStart.interactable = !EGSocial._IsAuthenticating; // 시도 중이 아닐 때 가능
-        _btnTextStart.text = EGSocial._IsAuthenticating ? _textSignInDoing : _textStart;
+        _strEvtStart.StringReference.SetReference(StringTableName._ui, EGSocial._IsAuthenticating ? _strKeySignInDoing : _strKeyStart);
 
         // 업적 버튼
         _btnAchievement.interactable = EGSocial._IsAuthenticated; // 로그인 되었을 때 가능
